fcf4b7921bf9a33f59300b8208d826d1c1117471
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-06 13:57:46 -0400

    Added a bit of code to start it off

---
+++ src/BmiProject.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BmiProject {
+
+    public static void main(String[] args){
+        //Declare
+        String feet;
+        feet =
+
+        Scanner keyboard = new Scanner(System.in);
+
+
+        BMI = kg/m*m
+        m = inch *(0.0254meters/1inch)
+        inch = m*(1inch/0.0254meters)
+
+        //Collecting info
+        System.out.println("Whats is your height?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
